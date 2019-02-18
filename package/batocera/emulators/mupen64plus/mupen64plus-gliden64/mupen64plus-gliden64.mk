804eb019cdb976c6b2b8fb552c093bd3f948fc32
mightyllama89 <31262390+mightyllama89@users.noreply.github.com>
2019-02-18 04:25:15 -0600

    update glideN64 to version 2.0

--- package/batocera/emulators/mupen64plus/mupen64plus-gliden64/mupen64plus-gliden64.mk
+++ package/batocera/emulators/mupen64plus/mupen64plus-gliden64/mupen64plus-gliden64.mk
@@ -4,17 +4,26 @@
 #
 ################################################################################
 
-MUPEN64PLUS_GLIDEN64_VERSION = 34fa719a61fa2338025facc9810ad2ef1a00e7ee # ok
+MUPEN64PLUS_GLIDEN64_VERSION = 72872963b8981e39dec66cfac77324f126c79e80 # release v2.0
 #MUPEN64PLUS_GLIDEN64_VERSION = 21f46db8693ae07dce93f03db129dbf4f7136823 ok bug buggy after 1 or 2 1080 course
 #MUPEN64PLUS_GLIDEN64_VERSION = 926e6c9225dd55efb317139029d84999cc9e11d3 ok bug buggy after 1 or 2 1080 course
 MUPEN64PLUS_GLIDEN64_SITE = $(call github,gonetz,GLideN64,$(MUPEN64PLUS_GLIDEN64_VERSION))
 MUPEN64PLUS_GLIDEN64_LICENSE = MIT
 MUPEN64PLUS_GLIDEN64_DEPENDENCIES = sdl2 alsa-lib mupen64plus-core
-MUPEN64PLUS_GLIDEN64_CONF_OPTS = -DMUPENPLUSAPI=On
+MUPEN64PLUS_GLIDEN64_CONF_OPTS = -DMUPENPLUSAPI=On -DVEC4_OPT=On
 MUPEN64PLUS_GLIDEN64_SUBDIR = /src/
 
 ifeq ($(BR2_PACKAGE_RPI_USERLAND),y)
 	MUPEN64PLUS_GLIDEN64_DEPENDENCIES += rpi-userland
+	MUPEN64PLUS_GLIDEN64_CONF_OPTS += -DGLES2=On -DNEON_OPT=On
+endif
+
+ifeq ($(BR2_PACKAGE_RECALBOX_TARGET_XU4),y)
+	MUPEN64PLUS_GLIDEN64_CONF_OPTS += -DGLES2=On -DNEON_OPT=On
+endif
+
+ifeq ($(BR2_PACKAGE_RECALBOX_TARGET_C2),y)
+	MUPEN64PLUS_GLIDEN64_CONF_OPTS += -DGLES2=On
 endif
 
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_S905)$(BR2_PACKAGE_BATOCERA_TARGET_S912)$(BR2_PACKAGE_BATOCERA_TARGET_C2)$(BR2_PACKAGE_BATOCERA_TARGET_XU4)$(BR2_PACKAGE_BATOCERA_TARGET_LEGACYXU4)$(BR2_PACKAGE_BATOCERA_TARGET_ROCKPRO64),y)
@@ -24,12 +33,15 @@ endif
 define MUPEN64PLUS_GLIDEN64_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/recalbox/configs/mupen64/
 	mkdir -p $(TARGET_DIR)/usr/share/mupen64plus/
+	mkdir -p $(TARGET_DIR)/recalbox/share_init/system/configs/mupen64/
 	$(INSTALL) -D $(@D)/src/plugin/release/mupen64plus-video-GLideN64.so \
 		$(TARGET_DIR)/usr/lib/mupen64plus/mupen64plus-video-gliden64.so
 	$(INSTALL) -D $(@D)/ini/* \
 		$(TARGET_DIR)/usr/share/mupen64plus/
 	$(INSTALL) -D $(@D)/ini/* \
 		$(TARGET_DIR)/recalbox/configs/mupen64/
+	$(INSTALL) -D $(@D)/ini/* \
+		$(TARGET_DIR)/recalbox/share_init/system/configs/mupen64/
 endef
 
 define MUPEN64PLUS_GLIDEN64_PRE_CONFIGURE_FIXUP
@@ -37,6 +49,8 @@ define MUPEN64PLUS_GLIDEN64_PRE_CONFIGURE_FIXUP
 	sh $(@D)/src/getRevision.sh
 	$(SED) 's|/opt/vc/include|$(STAGING_DIR)/usr/include|g' $(@D)/src/CMakeLists.txt
 	$(SED) 's|/opt/vc/lib|$(STAGING_DIR)/usr/lib|g' $(@D)/src/CMakeLists.txt
+	#must be removed during the next bump and use -DUSE_SYSTEM_LIBS=On option (not available in the public release 2.0)
+	$(SED) 's|elseif(BCMHOST)|elseif(UNIX OR BCMHOST)|g' $(@D)/src/GLideNHQ/CMakeLists.txt
 
 endef
 
